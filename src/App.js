b7de24f25f53d645bc50f62b2c11e387205cf324
Abhishek <abhishekparmar162@gmail.com>
2022-09-26 18:06:40 +0530

    added hero banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from './components/Hero'
 import NavBar from './components/NavBar'
 const App = () => {
   return (
     <div>
+    
       <NavBar />
+      <Hero />
     </div>
   )
 }
