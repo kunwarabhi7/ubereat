1e611815590f8eb42c7360a6ef32299679423d99
Abhishek <abhishekparmar162@gmail.com>
2022-09-26 16:19:03 +0530

    added sidebar icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import {MdOutlineRestaurantMenu} from "react-icons/md";
 import { FcSearch } from "react-icons/fc";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineShoppingCart ,AiFillTags} from "react-icons/ai";
+import { GiCrossedSwords } from "react-icons/gi";
+import { BsTruck,BsFillHeartFill,BsFillSaveFill } from "react-icons/bs";
+import { MdHelp} from "react-icons/md";
+import { FaWallet,FaUserFriends } from "react-icons/fa";
+
+
+
+
 
 
 
@@ -14,7 +22,7 @@ const NavBar = () => {
         <div className='cursor-pointer hover:bg-blue-300 bg-grey-300' >
         <MdOutlineRestaurantMenu size={40}/>
         </div>
-            <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Uber <span className='font-bold'>Eats</span></h1>
+            <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Uber <span className='font-bold text-green-600'>Eats</span></h1>
         <div className='flex '>
             <p className='bg-black text-white rounded-full p-2'>Delivery</p>
             <p className='bg-gray-400 rounded-full p-2 '>Pickup</p>
@@ -28,7 +36,24 @@ const NavBar = () => {
             
         </div>
         <button className='bg-black text-white text-bold hidden hover:bg-blue-300 hover:text-black md:flex items-center p-2 rounded-full'><AiOutlineShoppingCart className='mr-2 p-2' size={35} />Cart</button>
-            
+            {/* Mobile View */}
+            {/* OverLay */}
+            {/*SideBar */}
+            <div className='fixed top-0 left-0 w-[300px] h-screen z-10 bg-white duration-300'>
+<GiCrossedSwords size={30} className='absolute top-4 right-4 cursor-pointer'/>
+<h1 className='text-3xl p-4'>Uber <span className='font-bold text-green-600'>Eats</span></h1>
+<nav>
+  <ul className='flex flex-col text-gray-800'>
+    <li className='text-xl py-4 flex'><BsTruck size={35} className='mr-4 ml-2' />Orders</li>
+    <li className='text-xl py-4 flex'><BsFillHeartFill size={35} className='mr-4 ml-2' />Favorites</li>
+    <li className='text-xl py-4 flex'><FaWallet size={35} className='mr-4 ml-2' />Wallet</li>
+    <li className='text-xl py-4 flex'><MdHelp size={35} className='mr-4 ml-2' />Help</li>
+    <li className='text-xl py-4 flex'><AiFillTags size={35} className='mr-4 ml-2' />Promotions</li>
+    <li className='text-xl py-4 flex'><BsFillSaveFill size={35} className='mr-4 ml-2' />Best One</li>
+    <li className='text-xl py-4 flex'><FaUserFriends size={35} className='mr-4 ml-2' />Invite Friends</li>
+  </ul>
+</nav>
+            </div>
     </div>
   )
 }
